2ede03dc02514a05b1df38545c7bc2e5fc740366
KANG DONG WON <omnipotentmrk@gmail.com>
2025-04-03 00:48:07 +0900

    Product 에러코드 재정의

--- src/main/java/com/musinsa/homework/enums/ProductErrorType.java
+++ src/main/java/com/musinsa/homework/enums/ProductErrorType.java
@@ -1,8 +1,11 @@
 package com.musinsa.homework.enums;
 
 public enum ProductErrorType implements ErrorType {
-    CANNOT_ADD_WITH_NOT_EXIST_CATEGORY("P0001", "존재하지 않는 카테고리로의 상품 추가는 불가능 합니다."),
-    NOT_EXIST("P0002", "존재하지 않는 상품 입니다.");
+    NOT_EXIST("P0001", "존재하지 않는 상품 입니다."),
+    CANNOT_ADD_WITH_NOT_EXIST_BRAND("P0002", "존재하지 않는 브랜드로 상품 추가는 불가능 합니다."),
+    CANNOT_ADD_WITH_NOT_EXIST_CATEGORY("P0003", "존재하지 않는 카테고리로 상품 추가는 불가능 합니다."),
+    CANNOT_MODIFY_WITH_NOT_EXIST_BRAND("P0004", "존재하지 않는 브랜드로 상품 수정은 불가능 합니다."),
+    CANNOT_MODIFY_WITH_NOT_EXIST_CATEGORY("P0005", "존재하지 않는 카테고리로 상품 수정은 불가능 합니다.");
 
     private final String code;
     private final String message;
